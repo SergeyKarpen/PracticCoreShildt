98dd5a02ee662c66014f3272751ff3a16c1ebf33
Sergey Karpenchyk <karp.en@inbox.ru>
2020-06-22 19:28:26 +0300

    Add Task_2

--- src/main/java/com/sergeykarpen/javacore/chaprer18/practice_arrays/Task_2.java
+++ src/main/java/com/sergeykarpen/javacore/chaprer18/practice_arrays/Task_2.java
@@ -1,30 +1,59 @@
 package main.java.com.sergeykarpen.javacore.chaprer18.practice_arrays;
 
-
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
 
 /**
  * Дан массив целых чисел.Необходимо реализовать метод, которые определяет, содержит ли данный массив дубликаты
  */
 public class Task_2 {
 
-    static Integer[] defaultArray = {4, 5, 6, 6, 7, 8, 8, 8};
+    static int[] defaultArray = {4, 5, 6, 7, 5, 7};
+    static int numberIteration;
+    static ArrayList<Integer> list = new ArrayList<>();
+    static boolean aBoolean = false;
 
     public static void main(String[] args) {
 
-        final boolean[] booleans = new boolean[defaultArray.length];
-        for (int i = 0; i < defaultArray.length - 1; i++) {
-            while (booleans[i]) i++;
-            System.out.print(defaultArray[i] + " is repeat in: " + i + " ");
-            for (int j = i + 1; j < defaultArray.length; j++) {
-                if (defaultArray[i] == defaultArray[j]) {
-                    booleans[j] = true;
-                    System.out.print(j + " ");
+        addIntToArray(list);
+        searchMatches(list);
+        if (aBoolean) {
+            System.out.println(true);
+        } else System.out.println(false + "\n" + "Совпадений не найдено");
+    }
+
+
+    /**
+     *
+     * @param list
+     * @return true если число повторяется больше 1 раза в массивеж
+     */
+    public static boolean searchMatches(ArrayList<Integer> list) {
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = i; j < defaultArray.length; j++) {
+                if (list.get(i).equals(defaultArray[j])) {
+                    numberIteration++;
+                    if (numberIteration > 1) {
+                        aBoolean = true;
+                        System.out.println("Число " + list.get(i) + " повторяется " + numberIteration + " раз");
+                    }
                 }
             }
+            numberIteration = 0;
+
         }
-        System.out.println();
+        return aBoolean;
     }
-}
+
+
+    /**
+     *
+     * @param list пробеагем по исходному массиву и добавляем в list все значения которые не повторяются;
+     */
+    public static void addIntToArray(ArrayList<Integer> list) {
+        for (int value : defaultArray) {
+            if (!list.contains(value)) {
+                list.add(value);
+            }
+        }
+    }
 }
